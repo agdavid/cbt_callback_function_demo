27d14e64f4cc54db98629d273a71d1e69e286b9c
agdavid <antonio.g.david@gmail.com>
2016-09-21 09:02:00 -0400

    add third, further nested callback ThirdFunction

--- js/app.js
+++ js/app.js
@@ -2,10 +2,17 @@ function FirstFunction(a, b, DoSecond) {
   var c = a + b;
   alert('The FIRST function provides the sum: ' + c.toString());
     
-  DoSecond(a, b);
+  DoSecond(a, b, ThirdFunction);
 };
 
-function SecondFunction(a, b) {
+function SecondFunction(a, b, DoThird) {
   var c = a - b;
   alert('The SECOND function provides the difference: ' + c.toString());
+
+  DoThird(a, b);
+};
+
+function ThirdFunction(a, b) {
+  var c = a * b;
+  alert('The THIRD function provides the product: ' + c.toString());
 }; 
